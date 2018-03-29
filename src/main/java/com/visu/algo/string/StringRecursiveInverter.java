4754a56b697c1b32509edad024d8ad778d5b9a9d
Viktor Sukharev <viktor.suhareff@yandex.ru>
2018-03-29 17:04:14 +0300

    recursive string inverter

---
+++ src/main/java/com/visu/algo/string/StringRecursiveInverter.java
@@ -0,0 +1,23 @@
+package com.visu.algo.string;
+
+public class StringRecursiveInverter {
+
+    public String invert(String str) {
+        return invert(str.toCharArray(), 0);
+    }
+
+    private String invert(char[] str, int index) {
+        if (index == str.length / 2) {
+            return String.valueOf(str);
+        }
+
+        swap(str, index, str.length - 1 - index);
+        return invert(str, ++index);
+    }
+
+    private void swap(char[] str, int a, int b) {
+        char temp = str[a];
+        str[a] = str[b];
+        str[b] = temp;
+    }
+}
---
+++ src/test/java/com/visu/algo/string/StringRecursiveInverterTest.java
@@ -0,0 +1,29 @@
+package com.visu.algo.string;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class StringRecursiveInverterTest {
+
+    private final StringRecursiveInverter stringRecursiveInverter = new StringRecursiveInverter();
+
+    @Test
+    public void testInvertSingleSymbol() {
+        Assert.assertEquals("a", stringRecursiveInverter.invert("a"));
+    }
+
+    @Test
+    public void testInvertTwoSymbols() {
+        Assert.assertEquals("ba", stringRecursiveInverter.invert("ab"));
+    }
+
+    @Test
+    public void testInvertEven() {
+        Assert.assertEquals("321cba", stringRecursiveInverter.invert("abc123"));
+    }
+
+    @Test
+    public void testInvertOdd() {
+        Assert.assertEquals("4321cba", stringRecursiveInverter.invert("abc1234"));
+    }
+}
