cce32b5bae5fff0147b880418623d6cd0356560c
ViktorSuharev <viktor.suhareff@yandex.ru>
2018-03-29 22:33:45 +0300

    binary search

---
+++ src/main/java/com/visu/algo/search/binary/BinarySearcher.java
@@ -0,0 +1,20 @@
+package com.visu.algo.search.binary;
+
+public class BinarySearcher {
+
+    public int binarySearch(double[] elements, double xElement) {
+        return search(elements, 0, elements.length - 1, xElement);
+    }
+
+    private int search(double[] elements, int left, int right, double xElement) {
+        if (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (elements[mid] == xElement) return mid;
+            else if (elements[mid] > xElement) return search(elements, left, mid - 1, xElement);
+            else return search(elements, mid + 1, right, xElement);
+        }
+
+        return -1;
+    }
+}
